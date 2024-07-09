e5c9fe12772cfdc0053ce3c3a56ceb2c1cec13f7
sara <sara.sharifirad@gmail.com>
2024-07-09 13:34:36 +0430

    session 3 added.

--- 3-email-pattern.py
+++ 3-email-pattern.py
@@ -3,7 +3,7 @@ import re
 print("<Email Address Pattern>")
 email = input("Enter your email: ")
 
-if re.match(r"(^[^,_.0-9]+)[a-zA-Z0-9_.]{6,30}@(gmail|yahoo|msn)\.com", email):
+if re.match(r"([^,_.0-9]+)[a-zA-Z0-9_.]{6,30}@(gmail|yahoo|msn)\.com", email):
     print("OK", email)
 else:
     print("Invalid email address.")
--- 3-name-pattern.py
+++ 3-name-pattern.py
@@ -3,7 +3,7 @@ import re
 print("<Name Pattern>")
 name = input("Enter your name: ")
 
-if re.match(r"([^_.,0-9@\$!\-+#*]+)[a-zA-Z\s]{2,}", name):
+if re.match(r"([^_.,0-9@$!\-+#*]+)[a-zA-Z\s]{2,}", name):
     print("OK", name)
 else:
     print("Invalid name.")
